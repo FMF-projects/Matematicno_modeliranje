1f35072cdcb7d7657beed47fea833b761a63216d
Tinka Pirc <tinka.pirc@gmail.com>
2020-06-16 21:50:14 +0200

    vaje 8:dopolnitev

--- vaje_8/isci_theta.m
+++ vaje_8/isci_theta.m
@@ -3,10 +3,10 @@ function [theta, k] = isci_theta(T2)
 
 b = T2(1);
 B = T2(2);
-f = @(x) 1 - cos(x) + B/b .* (x - sin(x));
+f = @(x) 1 - cos(x) + B/b .* (x - sin(x)); %stran 2
 
 theta = fzero(f,1);
 
-k = sqrt(2*b / (theta - sin(theta)));
+k = sqrt(2*b / (theta - sin(theta))); %stran 3
 end
 
